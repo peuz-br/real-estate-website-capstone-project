80cc1c1da6244f695f376bd993b2dbb5fb8dff48
pedrohschool@gmail.com <pedrohschool@gmail.com>
2024-10-22 15:52:59 -0300

    updated server.js - mysql

--- back-end/server.js
+++ back-end/server.js
@@ -24,3 +24,25 @@ app.get('/', (req, res) => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+
+// mysql connect
+
+const mysql = require('mysql2');
+
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.error('error connecting to MySQL:', err);
+    return;
+  }
+  console.log('connected to MySQL Database');
+});
