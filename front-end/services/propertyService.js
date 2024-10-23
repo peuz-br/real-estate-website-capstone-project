b3804d5138770934dfb76b01270a6a72b7cfbcd0
pedrohschool@gmail.com <pedrohschool@gmail.com>
2024-10-22 21:37:31 -0300

    updated propertyService.js

--- front-end/services/propertyService.js
+++ front-end/services/propertyService.js
@@ -10,3 +10,25 @@ export const fetchProperties = async () => {
     throw error;
   }
 };
+
+// create
+export const createProperty = async (propertyData) => {
+    try {
+      const token = localStorage.getItem('token'); 
+  
+      const response = await fetch(`${API_URL}/properties`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`, 
+        },
+        body: JSON.stringify(propertyData),
+      });
+      return await response.json();
+    } catch (error) {
+      console.error('Error creating property:', error);
+      throw error;
+    }
+  };
+
+  
