b1d9db838d843a51f0ac79fd2ba51891f3abdcb6
pedrohschool@gmail.com <pedrohschool@gmail.com>
2024-10-22 21:21:13 -0300

    updated authService,js

---
+++ front-end/services/authService.js
@@ -0,0 +1,21 @@
+const API_URL = 'http://localhost:5000';
+
+
+//registration
+
+export const registerUser = async (userData) => {
+  try {
+    const response = await fetch(`${API_URL}/users/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error('Error registering user:', error);
+    throw error;
+  }
+};
+
